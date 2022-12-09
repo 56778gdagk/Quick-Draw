1973900c30c00b3f676aad97053a36849c41f94b
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2022-12-09 19:05:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,47 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+sketch = Element_of_array;
+
+function draw()
+{
+    check_sketch();
+    if(drawn_sketch == sketch)
+    {
+        answer_holder = "set";
+        score = score+1;
+        document.getElementById("score").innerHTML = "score : " + score;
+
+    }
+}
+
+function check_sketch()
+{
+    timer_count++;
+    document.getElementById("timer").innerHTML = "timer :"+timer_count;
+    if(timer_count>4000)
+    {
+        timer_count = 0;
+        timer_check = "completed";
+    }
+    if(timer_check = "completed" || answer_holder =="set");
+    {
+        timer_check = "";
+        answer_holder = "";
+        update_canvas();
+    }
+}
+
+function update_canvas()
+{
+    background('white');
+    sketch = Element_of_array;
+    document.getElementById("sketch_draw").innerHTML = "Sketch To Be Drawn : " + sketch;
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.position(490,250);
+    background('white');
+}
